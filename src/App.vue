5b53e9ab3a8e52f93a2dd51c76f52d66490ee572
carolyangtw <carol.yang@nextfortune.tw>
2026-06-29 15:39:15 +0800

    App vue 修改

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,11 @@ import CheerModal from "./components/CheerModal.vue";
 </script>
 
 <template>
-  <main class="min-h-screen bg-gray-900 flex items-center justify-center p-6">
-    <!-- Phone frame -->
+  <main class="min-h-screen bg-gray-900 flex items-center justify-center p-0 md:p-6">
     <div
-      class="w-full max-w-sm md:max-w-6xl bg-black overflow-hidden shadow-2xl border border-gray-700"
+      class="relative w-full max-w-sm md:max-w-6xl bg-black overflow-hidden shadow-2xl border border-gray-700"
     >
-      <!-- Inner content: vertical on mobile, horizontal on md+ -->
       <div class="flex flex-col md:flex-row">
-        <!-- Video section -->
         <div class="w-full md:w-2/3">
           <img
             src="./assets/img/chatroom-video.png"
@@ -19,21 +16,19 @@ import CheerModal from "./components/CheerModal.vue";
             class="w-full h-full object-contain"
           />
         </div>
-        <!-- Chat section -->
-        <div class="w-full md:w-1/3">
+
+        <div class="relative w-full md:w-1/3">
           <img
             src="./assets/img/chatroom-content.png"
             alt="chatroom"
             class="w-full h-full object-cover"
           />
 
-          <!-- 排行榜 -->
-          <Ranking v-if="false"></Ranking>
-
-          <!-- 小彈窗 -->
           <CheerModal v-if="true"></CheerModal>
         </div>
       </div>
+
+      <Ranking v-if="false"></Ranking>
     </div>
   </main>
 </template>
