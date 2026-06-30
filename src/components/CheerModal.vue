6889455e84454f2f5061b73bb9879d3b516a91b0
carolyangtw <carol.yang@nextfortune.tw>
2026-06-30 10:53:28 +0800

    字型大小寫法統一

--- src/components/CheerModal.vue
+++ src/components/CheerModal.vue
@@ -5,10 +5,9 @@ import coffeeSrc from '../assets/img/coffee.png'
 
 <template>
   <div
-    role="alert"
-    class="cheer-modal-enter absolute left-[12px] right-[12px] bottom-[60px] z-50 flex min-h-[56px] items-center gap-5 rounded-lg border border-red-600 bg-[rgba(147,42,0,0.9)] p-2 text-white md:hidden"
+    class="cheer-modal-enter absolute left-[12px] right-[12px] bottom-[60px] z-50 flex min-h-[56px] items-center gap-5 rounded-lg border border-red-600 bg-[rgba(147,42,0,0.9)] p-2 font-['Roboto'] text-white md:hidden"
   >
-    <div class="flex min-w-0 flex-1 flex-col items-start gap-1 text-xs font-normal leading-[18px]">
+    <div class="flex min-w-0 flex-1 flex-col items-start gap-1 text-[12px] leading-[18px]">
       <p class="m-0 leading-[18px]">
         <span class="font-bold">Angel : </span>
         <span>恭喜大佬贏了一單！</span>
@@ -25,13 +24,13 @@ import coffeeSrc from '../assets/img/coffee.png'
         type="button"
       >
         <span class="flex min-h-6 w-6 flex-col items-center justify-center">
-          <span class="min-h-[14px] max-w-6 break-words text-center text-xs font-bold leading-[12px]">果汁</span>
-          <span class="min-h-2.5 text-center text-[10px] leading-[10px]">$5</span>
+          <span class="max-w-6 break-words text-center text-[12px] font-bold leading-[12px]">果汁</span>
+          <span class="text-center text-[10px] leading-[10px]">$5</span>
         </span>
 
         <span class="absolute -left-[10px] -top-[5px] size-[38px] overflow-hidden">
           <img
-            class="h-full w-full object-contain object-center"
+            class="h-full w-full object-contain"
             :src="juiceSrc"
             alt=""
           />
@@ -43,13 +42,13 @@ import coffeeSrc from '../assets/img/coffee.png'
         type="button"
       >
         <span class="flex min-h-6 w-6 flex-col items-center justify-center">
-          <span class="min-h-[14px] max-w-6 break-words text-center text-xs font-bold leading-[12px]">咖啡</span>
-          <span class="min-h-2.5 text-center text-[10px] leading-[10px]">$1</span>
+          <span class="max-w-6 break-words text-center text-[12px] font-bold leading-[12px]">咖啡</span>
+          <span class="text-center text-[10px] leading-[10px]">$1</span>
         </span>
 
         <span class="absolute -left-[10px] -top-[5px] size-[38px] overflow-hidden">
           <img
-            class="h-full w-full object-contain object-center"
+            class="h-full w-full object-contain"
             :src="coffeeSrc"
             alt=""
           />
